62bcad2117fb105c80b48daeeb0f98ac9fe8774e
Elise Kain <elise.kain@gmail.com>
2015-11-25 13:47:15 -0500

    Refactor into getGiphy function and remove li bullets

--- js/giphy.js
+++ js/giphy.js
@@ -1,48 +1,35 @@
-$(document).ready(function(){
+var getGiphy = function(offset) {
+  alreadyLoading = true;
+  var query = encodeURIComponent($("input").val());
+  var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + offset;
+  $.ajax({
+    url: url,
+    type: "get",
+    dataType: "json"
+  }).done(function(response){
+    $.each(response.data, function(key, value) {
+      $(".giphy_container").append("<li><img src=" + value.images.fixed_height.url + "></li>")
+    })
+    alreadyLoading = false;
+  }).fail(function(){
+    console.log("Ajax request fails!");
+  })
+}
 
+$(document).ready(function(){
+  // Initial query
   $("button").on("click", function(e){
     e.preventDefault();
-    var query = $("input").val();
-    query = encodeURIComponent(query);
-    var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10";
-    $.ajax({
-      url: url,
-      type: "get",
-      dataType: "json"
-    }).done(function(response){
-      console.log(response)
-      $.each(response.data, function(key, value) {
-        $(".giphy_container").append("<li><img src=" + value.images.fixed_height.url + "></li>")
-      })
-    }).fail(function(){
-      console.log("Ajax request fails!");
-    })
+    getGiphy(0);
   })
 })
 
-  var alreadyLoading = false;
-
+  // Add 10 more giphys when scrolled to bottom of page
   $(window).scroll(function () {
-    var offset = $(".giphy_container").children().length;
-    if ($(window).height() + $(window).scrollTop() >= $(document).height() - 200) {
-      if (alreadyLoading == false) {
-        alreadyLoading = true;
-        var query = $("input").val();
-        query = encodeURIComponent(query);
-        var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + offset;
-        $.ajax({
-          url: url,
-          type: "get",
-          dataType: "json"
-        }).done(function(response){
-          console.log(response)
-          $.each(response.data, function(key, value) {
-            $(".giphy_container").append("<li><img src=" + value.images.fixed_height.url + "></li>")
-            alreadyLoading = false;
-          })
-        }).fail(function(){
-          console.log("Ajax request fails!");
-        })
-      }
+    var offsetNow = $(".giphy_container").children().length;
+    var currentScroll = $(window).height() + $(window).scrollTop();
+    var triggerPoint = $(document).height() - 200;
+    if ((currentScroll >= triggerPoint) && (alreadyLoading == false)) {
+        getGiphy(offsetNow);
     }
   });
