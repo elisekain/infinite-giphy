97e3564028aca169023ff07f1a7108e51a9c45be
Elise Kain <elise.kain@gmail.com>
2015-11-25 12:59:31 -0500

    Get first 10 images with giphy

--- js/giphy.js
+++ js/giphy.js
@@ -3,14 +3,16 @@ $(document).ready(function(){
     e.preventDefault();
     var query = $("input").val();
     query = encodeURIComponent(query);
-    var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC";
+    var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10";
     $.ajax({
       url: url,
       type: "get",
       dataType: "json"
     }).done(function(response){
-      console.log(response);
-      console.log(query);
+      console.log(response)
+      $.each(response.data, function(key, value) {
+        $("body").append("<div><img src=" + value.images.fixed_height.url + "></div>")
+      })
     }).fail(function(){
       console.log("Ajax request fails!");
     })
