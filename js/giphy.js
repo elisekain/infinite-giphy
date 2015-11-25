56f6bb92a7c9e510e00291478827631cabd1c06e
Elise Kain <elise.kain@gmail.com>
2015-11-25 13:19:12 -0500

    Add window scroll

--- js/giphy.js
+++ js/giphy.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   $("button").on("click", function(e){
     e.preventDefault();
     var query = $("input").val();
@@ -11,10 +12,31 @@ $(document).ready(function(){
     }).done(function(response){
       console.log(response)
       $.each(response.data, function(key, value) {
-        $("body").append("<div><img src=" + value.images.fixed_height.url + "></div>")
+        $(".giphy_container").append("<li><img src=" + value.images.fixed_height.url + "></li>")
       })
     }).fail(function(){
       console.log("Ajax request fails!");
     })
   })
 })
+
+  $(window).scroll(function () {
+    var offset = $(".giphy_container").children().length;
+    if ($(window).height() + $(window).scrollTop() >= $(document).height() - 200) {
+      var query = $("input").val();
+      query = encodeURIComponent(query);
+      var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + offset;
+      $.ajax({
+        url: url,
+        type: "get",
+        dataType: "json"
+      }).done(function(response){
+        console.log(response)
+        $.each(response.data, function(key, value) {
+          $(".giphy_container").append("<li><img src=" + value.images.fixed_height.url + "></li>")
+        })
+      }).fail(function(){
+        console.log("Ajax request fails!");
+      })
+    }
+  });
