bd9d09ba17b135f35c06d8750582bcf967f23c34
Elise Kain <elise.kain@gmail.com>
2015-11-25 13:29:56 -0500

    Add alreadyLoading check

--- js/giphy.js
+++ js/giphy.js
@@ -20,23 +20,29 @@ $(document).ready(function(){
   })
 })
 
+  var alreadyLoading = false;
+
   $(window).scroll(function () {
     var offset = $(".giphy_container").children().length;
     if ($(window).height() + $(window).scrollTop() >= $(document).height() - 200) {
-      var query = $("input").val();
-      query = encodeURIComponent(query);
-      var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + offset;
-      $.ajax({
-        url: url,
-        type: "get",
-        dataType: "json"
-      }).done(function(response){
-        console.log(response)
-        $.each(response.data, function(key, value) {
-          $(".giphy_container").append("<li><img src=" + value.images.fixed_height.url + "></li>")
+      if (alreadyLoading == false) {
+        alreadyLoading = true;
+        var query = $("input").val();
+        query = encodeURIComponent(query);
+        var url = "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=dc6zaTOxFJmzC&limit=10&offset=" + offset;
+        $.ajax({
+          url: url,
+          type: "get",
+          dataType: "json"
+        }).done(function(response){
+          console.log(response)
+          $.each(response.data, function(key, value) {
+            $(".giphy_container").append("<li><img src=" + value.images.fixed_height.url + "></li>")
+            alreadyLoading = false;
+          })
+        }).fail(function(){
+          console.log("Ajax request fails!");
         })
-      }).fail(function(){
-        console.log("Ajax request fails!");
-      })
+      }
     }
   });
